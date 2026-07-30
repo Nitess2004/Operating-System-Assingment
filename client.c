d2f03dfad4b632b3a4e05f7b7bce983b1260195f
Nitess2004 <katuwalnitess3@gmail.com>
2026-07-30 23:24:28 +0545

    code edited for client

--- client.c
+++ client.c
@@ -1,35 +1,59 @@
-#include<stdio.h>
-#include<string.h>
-#include<unistd.h>
-#include<arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-int main(){
+#define PORT 8080
+#define SERVER_IP "127.0.0.1"
+#define MAX_FIELD 30
+#define MAX_MSG 128
 
-    int s;
-    struct sockaddr_in server;
-    char user[30],pass[30],msg[100],reply[100];
+int main(void) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) { perror("socket failed"); return 1; }
 
-    s=socket(AF_INET,SOCK_STREAM,0);
+    struct sockaddr_in server = {0};
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
 
-    server.sin_family=AF_INET;
-    server.sin_port=htons(8080);
-    server.sin_addr.s_addr=inet_addr("127.0.0.1");
+    if (inet_pton(AF_INET, SERVER_IP, &server.sin_addr) <= 0) {
+        fprintf(stderr, "Invalid server address\n");
+        close(sock);
+        return 1;
+    }
 
-    connect(s,(struct sockaddr*)&server,sizeof(server));
+    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        perror("connect failed - is the server running?");
+        close(sock);
+        return 1;
+    }
 
+    char user[MAX_FIELD], pass[MAX_FIELD];
     printf("Username: ");
-    scanf("%s",user);
-
+    if (scanf("%29s", user) != 1) { close(sock); return 1; }
     printf("Password: ");
-    scanf("%s",pass);
+    if (scanf("%29s", pass) != 1) { close(sock); return 1; }
 
-    sprintf(msg,"%s %s",user,pass);
+    char msg[MAX_MSG];
+    snprintf(msg, sizeof(msg), "AUTH %s %s", user, pass);
 
-    send(s,msg,strlen(msg),0);
+    if (send(sock, msg, strlen(msg), 0) < 0) {
+        perror("send failed");
+        close(sock);
+        return 1;
+    }
 
-    recv(s,reply,sizeof(reply),0);
+    char reply[MAX_MSG];
+    ssize_t n = recv(sock, reply, sizeof(reply) - 1, 0);
+    if (n < 0) {
+        perror("recv failed");
+        close(sock);
+        return 1;
+    }
+    reply[n] = '\0'; 
 
-    printf("Server: %s\n",reply);
+    printf("Server says: %s", reply);
 
-    close(s);
+    close(sock);
+    return 0;
 }
